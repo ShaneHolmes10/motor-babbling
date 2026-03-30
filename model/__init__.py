e2bcc7845325fd75628fb19a2b9d1493d7eca46f
shaneholmes10 <shaneholmes2010@gmail.com>
2026-03-30 19:23:46 -0400

    Implemented the sac agent

--- model/__init__.py
+++ model/__init__.py
@@ -1,13 +1,14 @@
 from .dqn_agent import QNetwork, DQNAgent
 from .ddpg_agent import Actor, Critic, DDPGAgent
+from .sac_agent import SACAgent
 from .replay_buffer import ReplayBuffer
 
 __all__ = [
     "QNetwork",
-    "ReplayBuffer",
     "DQNAgent",
     "Actor",
     "Critic",
     "DDPGAgent",
-    "ReplayBuffer"
+    "SACAgent",
+    "ReplayBuffer",
 ]
