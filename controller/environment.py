dc7e6e1937c9950336454af6b8a33dc6a2aa0783
shaneholmes10 <shaneholmes2010@gmail.com>
2026-04-01 14:55:31 -0400

    FIxed bug where random targets weren't a random distribution of targets in the configuration space of the robot

--- controller/environment.py
+++ controller/environment.py
@@ -73,7 +73,7 @@ class RobotReachingEnv(gym.Env):
         if options and options.get("random_target", False):
             angle = np.random.uniform(-np.pi / 2, np.pi / 2)
             max_reach = self.link_length * self.num_links
-            radius = max_reach
+            radius = np.random.uniform(0.1, max_reach)
             self.target = np.array(
                 [radius * np.sin(angle), 1.5 - radius * np.cos(angle)]
             )
